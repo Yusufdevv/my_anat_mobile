35adcc2f51efbc9fe7ae857e0c19f418e0080f4e
IkromovDilyor <dilyor6130@gmail.com>
2022-08-11 18:01:12 +0500

    feat: implement vacancy screen (in progress)

---
+++ lib/features/vacancy/prezentation/widgets/filter_card.dart
@@ -0,0 +1,33 @@
+import 'package:anatomica/assets/colors/colors.dart';
+import 'package:anatomica/assets/constants/app_icons.dart';
+import 'package:anatomica/features/common/presentation/widgets/w_scale_animation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class FilterCard extends StatelessWidget {
+  const FilterCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: primary,
+      ),
+      child: Row(
+        children: [
+          Text(
+            'До 3 000 000',
+            style: Theme.of(context).textTheme.headline2!.copyWith(
+                  fontSize: 13,
+                  fontWeight: FontWeight.w500,
+                ),
+          ),
+          const SizedBox(width: 8),
+          SvgPicture.asset(AppIcons.cancelButton),
+        ],
+      ),
+    );
+  }
+}
