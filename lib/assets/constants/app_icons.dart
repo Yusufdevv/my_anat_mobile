539e9fdcbabf9252259d7db746f241920c7d6d5d
Sirojiddin Odamboev <s.odamboev@uic.group>
2022-08-19 16:34:46 +0500

    rebase with master

--- lib/assets/constants/app_icons.dart
+++ lib/assets/constants/app_icons.dart
@@ -44,17 +44,13 @@ class AppIcons {
   static const filter = 'assets/icons/vacancy/filter.svg';
 
 //  static const star = 'assets/icons/vacancy/star.svg';
-
-  //static const star = 'assets/icons/vacancy/star.svg';
   static const mapPin = 'assets/icons/vacancy/map_pin.svg';
   static const briefCase = 'assets/icons/vacancy/briefcase.svg';
   static const cancelButton = 'assets/icons/vacancy/cancel_button.svg';
   static const cashBanknote = 'assets/icons/vacancy/cash_banknote.svg';
   static const vacancySingleStar = 'assets/icons/vacancy/vacancy_single_star.svg';
-  //static const share = 'assets/icons/vacancy/share.svg';
-  // static const phone = 'assets/icons/vacancy/phone.svg';
-  // static const share = 'assets/icons/vacancy/share.svg';
-  // static const phone = 'assets/icons/vacancy/phone.svg';
+  static const vacancyShare = 'assets/icons/vacancy/share.svg';
+  static const vacancyShone = 'assets/icons/vacancy/phone.svg';
   static const chevronLeft = 'assets/icons/vacancy/chevron_left.svg';
   static const calendar = 'assets/icons/vacancy/calendar.svg';
   static const numberList = 'assets/icons/vacancy/list_numbers.svg';
